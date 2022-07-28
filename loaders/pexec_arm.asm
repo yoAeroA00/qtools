66df6fb78bd65ef4826ca2209314f1fca68676dd
Maks-2012 <maks0-2012@ya.ru>
2022-07-28 15:14:22 +0300

    Поправил шаблоны патча загрузчиков

--- loaders/pexec_arm.asm
+++ loaders/pexec_arm.asm
@@ -1,29 +1,34 @@
 @
 @  Патч команды 11 для загрузчиков, работающих в режиме ARM
 @
-.syntax unified 
+.syntax unified
 
 pkt_data_len_off=8
-		.ORG		0x00127140
+
+.ORG    0xXXXXXXXX
+
 leavecmd:
 
-		.ORG		0x00127158
-cmd_11_exec:
-		PUSH		{R4,LR}
-		LDR		R1, reply_buf_ptr 	     @ Адрес ответнго буфера
-		ADD		R0, R0,	#8	             @ R0 теперь показывает на байт +4 от начала
-							     @ командного буфера
-		MCR 		p15, 0, R1,c7,c5,0           @ сброс I-кеша
-		BLX		R0               	     @ передаем туда управление
-		LDR		R0, escape_state_ptr         @  Структура данных командного обработчика
-		STRH		R4, [R0,#pkt_data_len_off]   @ сохраняем размер ответного буфера
-		B		leavecmd
-@ блок идентификации
-                .word 		0xdeadbeef   @ сигнатура
-                .byte           2            @ код чипсета
+.ORG    0xXXXXXXXX
 
-		.ORG  0x00127188
-		
+cmd_11_exec:
+		PUSH    {R4,LR}
+		LDR     R1, reply_buf_ptr               @ Адрес ответнго буфера
+		ADD     R0, R0,	#8                      @ R0 теперь показывает на байт +4 от начал
+                                                @ командного буфера
+		MCR     p15, 0, R1,c7,c5,0              @ сброс I-кеша
+		BLX     R0                              @ передаем туда управление
+		LDR     R0, escape_state_ptr            @  Структура данных командного обработчика
+		STRH    R4, [R0,#pkt_data_len_off]      @ сохраняем размер ответного буфера
+		B       leavecmd
+
+@ блок идентификации
+
+        .word           0xdeadbeef          @ сигнатура
+        .byte           X                   @ код чипсета
+
+.ORG    0xXXXXXXXX
+
 reply_buf_ptr:    .word  0
 escape_state_ptr: .word  0
 
--- loaders/pexec_thumb.asm
+++ loaders/pexec_thumb.asm
@@ -1,30 +1,35 @@
 @
 @  Патч команды 11 для загрузчиков, работающих в режиме Thumb-2
 @
-.syntax unified 
-.code 16           @  Thumb-2 
+.syntax unified
+.code 16           @  Thumb-2
 
 pkt_data_len_off=10
-		.ORG		0x0011607c
+
+.ORG    0xXXXXXXXX
+
 leavecmd:
 
-		.ORG		0x00116110
+.ORG    0xXXXXXXXX
+
 cmd_11_exec:
-		PUSH		{R4,LR}
-		LDR		R1, cmd_reply_code_ptr 	     @ Адрес ответнго буфера
-		ADD		R0, R0,	#8	             @ R0 теперь показывает на байт +4 от начала
-							     @ командного буфера
-		MCR 		p15, 0, R1,c7,c5,0           @ сброс I-кеша
-		BLX		R0               	     @ передаем туда управление
-		LDR		R0, cmd_processor_data_ptr   @  Структура данных командного обработчика
-		STRH		R4, [R0,#pkt_data_len_off]   @ сохраняем размер ответного буфера
-		B		leavecmd
+        PUSH    {R4,LR}
+        LDR     R1, cmd_reply_code_ptr          @ Адрес ответнго буфера
+        ADD     R0, R0,    #8                   @ R0 теперь показывает на байт +4 от начала
+                                                @ командного буфера
+        MCR     p15, 0, R1,c7,c5,0              @ сброс I-кеша
+        BLX     R0                              @ передаем туда управление
+        LDR     R0, cmd_processor_data_ptr      @  Структура данных командного обработчика
+        STRH    R4, [R0,#pkt_data_len_off]      @ сохраняем размер ответного буфера
+        B       leavecmd
+
 @ блок идентификации
-                .word 		0xdeadbeef   @ сигнатура
-                .byte           3            @ код чипсета
-		
-		.ORG  0x00116140
-		
-reply_buf_ptr:    .word  0
-escape_state_ptr: .word  0
+
+        .word           0xdeadbeef          @ сигнатура
+        .byte           X                   @ код чипсета
+
+.ORG    0xXXXXXXXX
+
+cmd_processor_data_ptr: .word  0
+cmd_reply_code_ptr:     .word  0
 
