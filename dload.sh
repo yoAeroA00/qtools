ff8ad43663a5bf1be8eeceaff2c05f6df916f051
Aditya Singh <yoAeroA00@users.noreply.github.com>
2023-11-02 18:40:59 +0530

    Translate *.sh : To English

--- dload.sh
+++ dload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# Скрипт для заливки загрузчика в модем, находящийся в стандартном режиме
+# Script for loading a bootloader into a modem in standard mode
 #
-# Параметр скрипта - имя порта tty, по умолчанию - /dev/ttyUSB0
+# Script parameter - tty port name, by default - /dev/ttyUSB0
 #
 PORT=$1
 if [ -z "$PORT" ]; then PORT=/dev/ttyUSB0; fi
@@ -13,30 +13,30 @@ if [ -z "$LDR" ]; then LDR=loaders/NPRG9x15p.bin; fi
 
 echo Diagnostic port: $PORT
 
-# Ждем появления порта в системе
+# Waiting for the port to appear on the system
 while [ ! -c $PORT ]
  do
   sleep 1
  done
 
-# команда переключения в download mode
+# download mode command
 echo entering download mode...
 ./qcommand -p $PORT -e -c "c 3a" >/dev/null
 
-# Ждем пропадания порта из системы
+# Waiting for the port to disappear from the system
 while [ -c $PORT ]
  do
   true
  done
 echo diagnostic port removed
 
-# Ждем появления нового порта, уже download
+# Waiting for the new port to arrive, already downloading
 while [ ! -c $PORT ]
  do
   sleep 1
  done
 
-# Заливаем загрузчик
+# Uploading the bootloader
 echo download mode entered
 sleep 2
 ./qdload -t -p $PORT -i  $LDR
