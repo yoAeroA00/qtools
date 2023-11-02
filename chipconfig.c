2cd41d04c840dd417a01a925bdc18d61b4f203b2
Aditya Singh <yoAeroA00@users.noreply.github.com>
2023-11-03 00:20:29 +0530

    Translate chipconfig.c : To English
    + Code Formatting and Proofreading.

--- chipconfig.c
+++ chipconfig.c
@@ -1,301 +1,298 @@
 //
-//  Процедуры работы с конфигурацией чипсета
+// Procedures for working with chipset configuration
 //
 #include "include.h"
 
-// Глбальные переменные - собираем их здесь
+// Global variables - we collect them here
 
-// тип чипcета:
-int chip_type=-1; // индекс текущего чипсета в таблице чипсетов 
-int maxchip=-1;   // число чипсетов в конфиге
+// Chipset type:
+int chip_type = -1; // Current chipset index in the chipset table
+int maxchip = -1;   // Number of chipsets in the configuration
 
-// описатели чипсетов
+// Chipset descriptors
 struct {
-  unsigned int id;         // код (id) чипcета
-  unsigned int nandbase;   // адрес контроллера
-  unsigned char udflag;    // udsize таблицы разделов, 0-512, 1-516
-  char name[20];  // имя чипсета
-  unsigned int ctrl_type;  // схема расположения регистров NAND-контроллера
-  unsigned int sahara;     // флаг sahara-протокола
-  char nprg[40];           // имя nprg-загрузчика
-  char enprg[40];         // имя enprg-загрузчика
-}  chipset[100];
+  unsigned int id;         // Chipset code (id)
+  unsigned int nandbase;   // Controller address
+  unsigned char udflag;    // udsize of the partition table, 0-512, 1-516
+  char name[20];  // Chipset name
+  unsigned int ctrl_type;  // NAND controller register layout
+  unsigned int sahara;     // Sahara protocol flag
+  char nprg[40];           // Name of the nprg bootloader
+  char enprg[40];         // Name of the enprg bootloader
+} chipset[100];
 
-// таблица кодов идентификации чипсета, не более 20 кодов на 1 чипсет
-unsigned short chip_code[100][20];  
+// Chipset identification code table, up to 20 codes per chipset
+unsigned short chip_code[100][20];
 
-// Адреса регистров чипcета (смещения относительно базы)
+// Controller register addresses (offsets relative to the base)
 struct {
   unsigned int nand_cmd;
   unsigned int nand_addr0;
   unsigned int nand_addr1;
-  unsigned int nand_cs;   
-  unsigned int nand_exec; 
+  unsigned int nand_cs;
+  unsigned int nand_exec;
   unsigned int nand_buffer_status;
   unsigned int nand_status;
-  unsigned int nand_cfg0;  
-  unsigned int nand_cfg1;  
+  unsigned int nand_cfg0;
+  unsigned int nand_cfg1;
   unsigned int nand_ecc_cfg;
-  unsigned int NAND_FLASH_READ_ID; 
+  unsigned int NAND_FLASH_READ_ID;
   unsigned int sector_buf;
-} nandreg[]={
-// cmd  adr0   adr1    cs    exec  buf_st fl_st  cfg0   cfg1   ecc     id   sbuf 
-  { 0,   4,     8,    0xc,   0x10 , 0x18, 0x14,  0x20  ,0x24,  0x28,  0x40, 0x100 }, //  ctrl=0 - новые
-  {0x304,0x300,0xffff,0x30c,0xffff,0xffff,0x308, 0xffff,0x328,0xffff, 0x320,0     }  //  ctrl=1 - старые
-};  
+} nandreg[] = {
+  // cmd  adr0   adr1    cs    exec  buf_st fl_st  cfg0   cfg1   ecc     id   sbuf
+  {0, 4, 8, 0xc, 0x10, 0x18, 0x14, 0x20, 0x24, 0x28, 0x40, 0x100}, // ctrl=0 - new
+  {0x304, 0x300, 0xffff, 0x30c, 0xffff, 0xffff, 0x308, 0xffff, 0x328, 0xffff, 0x320, 0}  // ctrl=1 - old
+};
 
-// Команды контроллера
+// Controller commands
 struct {
-  unsigned int stop;      // Остановка операций кнтроллера
-  unsigned int read;      // только данные
-  unsigned int readall;   // данные+ECC+spare
-  unsigned int program;    // только данные
-  unsigned int programall; // данные+ECC+spare
+  unsigned int stop;      // Stop controller operations
+  unsigned int read;      // Data only
+  unsigned int readall;   // Data + ECC + spare
+  unsigned int program;    // Data only
+  unsigned int programall; // Data + ECC + spare
   unsigned int erase;
   unsigned int identify;
 } nandopcodes[] = {
-//  stop   read  readall  program  programall erase  indetify
-  { 0x01,  0x32,  0x34,    0x36,     0x39,     0x3a,   0x0b },   // ctrl=0 - новые
-  { 0x07,  0x01,  0xffff,  0x03,   0xffff,     0x04,   0x05 }    // ctrl=1 - старые
-};  
+  //  stop   read  readall  program  programall erase  identify
+  {0x01, 0x32, 0x34, 0x36, 0x39, 0x3a, 0x0b},   // ctrl=0 - new
+  {0x07, 0x01, 0xffff, 0x03, 0xffff, 0x04, 0x05}    // ctrl=1 - old
+};
 
-// глобальные хранилища адресов кнтроллера
+// Global controller register addresses
 
 unsigned int nand_addr0;
 unsigned int nand_addr1;
-unsigned int nand_cs;   
-unsigned int nand_exec; 
+unsigned int nand_cs;
+unsigned int nand_exec;
 unsigned int nand_status;
 unsigned int nand_buffer_status;
-unsigned int nand_cfg0;  
-unsigned int nand_cfg1;  
+unsigned int nand_cfg0;
+unsigned int nand_cfg1;
 unsigned int nand_ecc_cfg;
-unsigned int NAND_FLASH_READ_ID; 
+unsigned int NAND_FLASH_READ_ID;
 unsigned int sector_buf;
 
-// глобальные хранилища кодов команд
+// Global command code storage
 
-unsigned int nc_stop,nc_read,nc_readall,nc_program,nc_programall,nc_erase,nc_identify;
+unsigned int nc_stop, nc_read, nc_readall, nc_program, nc_programall, nc_erase, nc_identify;
 
 //************************************************
-//*   Загрузка конфига чипсетов
+//*   Load chipset configuration
 //************************************************
 int load_config() {
-char line[300];
-char* tok1, *tok2;
-int index;
-int msmidcount;
+  char line[300];
+  char* tok1, *tok2;
+  int index;
+  int msmidcount;
 
-char vname[50];
-char vval[100];
+  char vname[50];
+  char vval[100];
 
-FILE *in = qopenfile("chipset.cfg", "r");
-if (in == 0) {
-  printf("\n! Файл конфигурации чипсетов chipset.cfg не найден\n");
-  return 0;  // конфиг не найден
-}
-
-while(fgets(line,300,in) != 0) {
-  if (strlen(line)<3) continue; // слишком короткая строка
-  if (line[0] == '#') continue; // комментарий
-  index=strspn(line," ");  // получаем указатель на начало информативной части строки
-  tok1=line+index;
-  
-  if (strlen(tok1) == 0) continue; // строка из одних пробелов
-  
-  // начало описателя очередного чипсета
-  if (tok1[0] == '[') {
-//     printf("\n@@ описатель чипсета:");
-   tok2=strchr(tok1,']');
-   if (tok2 == 0) {
-      printf("\n! Файл конфигурации содержит ошибку в заголовке чипсета\n %s\n",line);
-      return 0;
-    }   
-    tok2[0]=0;
-    // начинаем создание структуры описания очередного чипсета
-    maxchip++; // индекс в структуре
-    chipset[maxchip].id=0;         // код (id) чипcета 0 - несуществующий чипсет
-    chipset[maxchip].nandbase=0;   // адрес контроллера
-    chipset[maxchip].udflag=0;    // udsize таблицы разделов, 0-512, 1-516
-    strcpy(chipset[maxchip].name,tok1+1);  // имя чипсета
-    chipset[maxchip].ctrl_type=0;  // схема расположения регистров NAND-контроллера
-    chipset[maxchip].nprg[0]=0;
-    chipset[maxchip].enprg[0]=0;
-    memset(chip_code[maxchip],0xffff,40);  // таблица msm_id заполняется FF
-    msmidcount=0;
-//       printf("\n@@ %s",tok1+1);
-    continue;
+  FILE* in = qopenfile("chipset.cfg", "r");
+  if (in == 0) {
+    printf("\n! Chipset configuration file chipset.cfg not found\n");
+    return 0;  // Configuration not found
   }
 
+  while (fgets(line, 300, in) != 0) {
+    if (strlen(line) < 3) continue; // Too short line
+    if (line[0] == '#') continue; // Comment
+    index = strspn(line, " ");  // Get a pointer to the start of the informative part of the line
+    tok1 = line + index;
+
+    if (strlen(tok1) == 0) continue; // Line consists of spaces
+
+    // Start of the descriptor of the current chipset
+    if (tok1[0] == '[') {
+      //printf("\n@@ описатель чипсета:");
+      tok2 = strchr(tok1, ']');
+      if (tok2 == 0) {
+        printf("\n! The configuration file contains an error in the chipset header\n %s\n", line);
+        return 0;
+      }
+      tok2[0] = 0;
+      // Start creating the descriptor structure of the current chipset
+      maxchip++; // Index in the structure
+      chipset[maxchip].id = 0;         // Chipset code (id) 0 - non-existent chipset
+      chipset[maxchip].nandbase = 0;   // Controller address
+      chipset[maxchip].udflag = 0;    // udsize of the partition table, 0-512, 1-516
+      strcpy(chipset[maxchip].name, tok1 + 1);  // Chipset name
+      chipset[maxchip].ctrl_type = 0;  // NAND controller register layout
+      chipset[maxchip].nprg[0] = 0;
+      chipset[maxchip].enprg[0] = 0;
+      memset(chip_code[maxchip], 0xffff, 40);  // Fill the msm_id table with FF
+      msmidcount = 0;
+      //printf("\n@@ %s",tok1+1);
+      continue;
+    }
+
+    if (maxchip == -1) {
+      printf("\n! The configuration file contains lines outside the chipset description section\n");
+      return 0;
+    }
+
+    // The line is one of the variables describing the chipset
+    memset(vname, 0, sizeof(vname));
+    memset(vval, 0, sizeof(vval));
+    // Extract the variable descriptor
+    index = strspn(line, " ");  // Get a pointer to the start of the informative part of the line
+    tok1 = line + index; // Start of the token
+    index = strcspn(tok1, " ="); // End of the token
+    memcpy(vname, tok1, index); // Get the variable name
+
+    tok1 += index;
+    if (strchr(tok1, '=') == 0) {
+      printf("\n! Configuration file: no variable value\n%s\n", line);
+      return 0;
+    }
+    tok1 += strspn(tok1, "= "); // Skip the separator
+    strncpy(vval, tok1, strcspn(tok1, " \r\n"));
+
+    //printf("\n @@@ vname = <%s>   vval = <%s>",vname,vval);
+
+    // Parse variable names
+
+    // Chipset id
+    if (strcmp(vname, "id") == 0) {
+      chipset[maxchip].id = atoi(vval);         // Chipset code (id) 0 - non-existent chipset
+      if (chipset[maxchip].id == 0) {
+        printf("\n! Configuration file: id=0 is not allowed\n%s\n", line);
+        return 0;
+      }
+      continue;
+    }
+
+    // Controller address
+    if (strcmp(vname, "addr") == 0) {
+      sscanf(vval, "%x", &chipset[maxchip].nandbase);
+      continue;
+    }
+
+    // udflag
+    if (strcmp(vname, "udflag") == 0) {
+      chipset[maxchip].udflag = atoi(vval);
+      continue;
+    }
+
+    // Controller type
+    if (strcmp(vname, "ctrl") == 0) {
+      chipset[maxchip].ctrl_type = atoi(vval);
+      continue;
+    }
+
+    // msm_id table
+    if (strcmp(vname, "msmid") == 0) {
+      sscanf(vval, "%hx", &chip_code[maxchip][msmidcount++]);
+      continue;
+    }
+
+    // Sahara protocol flag
+    if (strcmp(vname, "sahara") == 0) {
+      chipset[maxchip].sahara = atoi(vval);
+      continue;
+    }
+
+    // Default NPRG bootloader name
+    if (strcmp(vname, "nprg") == 0) {
+      strncpy(chipset[maxchip].nprg, vval, 39);
+      continue;
+    }
+
+    // Default ENPRG bootloader name
+    if (strcmp(vname, "enprg") == 0) {
+      strncpy(chipset[maxchip].enprg, vval, 39);
+      continue;
+    }
+
+    // Other variable names
+    printf("\n! Configuration file: invalid variable name\n%s\n", line);
+    return 0;
+  }
+  fclose(in);
   if (maxchip == -1) {
-      printf("\n! Файл конфигурации содержит строки вне секции описания чипсетов\n");
-      return 0;
-  }   
-  
-  // строка является одной из переменных, описывающих чипсет
-  memset(vname,0,sizeof(vname));
-  memset(vval,0,sizeof(vval));
-  // выделяем токен-описатель
-  index=strspn(line," ");  // получаем указатель на начало информативной части строки
-  tok1=line+index; // начало токена
-  index=strcspn(tok1," ="); // конец токена
-  memcpy(vname,tok1,index); // получаем имя переменной
-  
-  tok1+=index; 
-  if (strchr(tok1,'=') == 0) {
-     printf("\n! Файл конфигурации: нет значения переменной\n%s\n",line);
-     return 0;
+    printf("\n! The configuration file does not contain any chipset descriptors\n");
+    return 0;
   }
-  tok1+=strspn(tok1,"= "); // пропускаем разделитель
-  strncpy(vval,tok1,strcspn(tok1," \r\n"));
-
-//   printf("\n @@@ vname = <%s>   vval = <%s>",vname,vval); 
-  
-  // разбираем имена переменных
-  
-  // id чипсета
-  if (strcmp(vname,"id") == 0) {
-     chipset[maxchip].id=atoi(vval);         // код (id) чипcета 0 - несуществующий чипсет
-     if (chipset[maxchip].id == 0) {
-      printf("\n! Файл конфигурации: id=0 недопустимо\n%s\n",line);
-      return 0;
-     }
-     continue;
-  }
-  
-  // адрес контроллера
-  if (strcmp(vname,"addr") == 0) {
-    sscanf(vval,"%x",&chipset[maxchip].nandbase);
-    continue;
-  }
-  
-  // udflag
-  if (strcmp(vname,"udflag") == 0) {
-    chipset[maxchip].udflag=atoi(vval);
-    continue;
-  }
-  
-  // тип контроллера
-  if (strcmp(vname,"ctrl") == 0) {
-    chipset[maxchip].ctrl_type=atoi(vval);
-    continue;
-  }
-
-  // таблица msm_id
-  if (strcmp(vname,"msmid") == 0) {
-    sscanf(vval,"%hx",&chip_code[maxchip][msmidcount++]);
-    continue;
-  }
-
-  // флаг sahara-протокола
-  if (strcmp(vname,"sahara") == 0) {
-    chipset[maxchip].sahara=atoi(vval);
-    continue;
-  }
-
-
-  // имя NPRG по умолчанию
-  if (strcmp(vname,"nprg") == 0) {
-  strncpy(chipset[maxchip].nprg,vval,39);
-  continue;
-  }
-  
-  // имя ENPRG по умолчанию
-  if (strcmp(vname,"enprg") == 0) {
-  strncpy(chipset[maxchip].enprg,vval,39);  
-  continue;
-  }
-  
-  // остальные имена
-  printf("\n! Файл конфигурации: недопустимое имя переменной\n%s\n",line);
-  return 0;
-
-} 
-fclose(in);
-if (maxchip == -1) {
-  printf("\n! Файл конфигурации не содержит ни одного описателя чипсетов\n");
-  return 0;
-}  
-maxchip++;
-return 1;
+  maxchip++;
+  return 1;
 }
 
-  
 //************************************************
-//*   Поиск чипсета по msm_id
+//*   Find a chipset by msm_id
 //************************************************
 int find_chipset(unsigned short code) {
-int i,j;
-if (maxchip == -1) 
-  if (!load_config()) exit(0); // конфиг не загрузился
-for(i=0;i<maxchip;i++) {
-  for (j=0;j<20;j++) {
-   if (code == chip_code[i][j]) return chipset[i].id;
-   if (chip_code[i][j] == 0xffff) break;
-  }    
+  int i, j;
+  if (maxchip == -1)
+    if (!load_config()) exit(0); // Configuration did not load
+  for (i = 0; i < maxchip; i++) {
+    for (j = 0; j < 20; j++) {
+      if (code == chip_code[i][j]) return chipset[i].id;
+      if (chip_code[i][j] == 0xffff) break;
+    }
+  }
+  // Not found
+  return -1;
 }
-// не найдено
-return -1;
-}  
 
 //************************************************
-//* Печать списка поддерживаемых чипсетов
-//***********************************************
+//* Print the list of supported chipsets
+//************************************************
 void list_chipset() {
-  
-int i,j;
-printf("\n Код     Имя    Адрес NAND   Тип  udflag  MSM_ID\n---------------------------------------------------------------------");
-for(i=0;i<maxchip;i++) {
-//  if (i == 0)  printf("\n  0 (по умолчанию) автоопределение чипсета");
-  printf("\n %2i  %9.9s    %08x    %1i     %1i    ",chipset[i].id,chipset[i].name,
-	 chipset[i].nandbase,chipset[i].ctrl_type,chipset[i].udflag);
-  for(j=0;chip_code[i][j]!=0xffff;j++) 
-    printf(" %04hx",chip_code[i][j]);
-}
-printf("\n\n");
-exit(0);
+
+  int i, j;
+  printf("\n Code     Name    NAND Address   Type  udflag  MSM_ID\n---------------------------------------------------------------------");
+  for (i = 0; i < maxchip; i++) {
+    //  if (i == 0)  printf("\n  0 (default) chipset auto-detection");
+    printf("\n %2i  %9.9s    %08x    %1i     %1i    ", chipset[i].id, chipset[i].name,
+      chipset[i].nandbase, chipset[i].ctrl_type, chipset[i].udflag);
+    for (j = 0; chip_code[i][j] != 0xffff; j++)
+      printf(" %04hx", chip_code[i][j]);
+  }
+  printf("\n\n");
+  exit(0);
 }
 
 //*******************************************************************************
-//*   Установка типа чипсета по коду чипсета из командной сроки
+//* Set the chipset type based on the chipset code from the command line
 //* 
-//* arg - указатель на optarg, указанный в ключе -к
+//* arg - pointer to optarg specified with the -k option
 //****************************************************************
 void define_chipset(char* arg) {
 
-unsigned int c;  
+  unsigned int c;
 
-if (maxchip == -1) 
-  if (!load_config()) exit(0); // конфиг не загрузился
-// проверяем на -kl
-if (optarg[0]=='l') list_chipset();
+  if (maxchip == -1)
+    if (!load_config()) exit(0); // Configuration did not load
+  // Check for -kl
+  if (optarg[0] == 'l') list_chipset();
 
-// получаем код чипсета из аргумента
-sscanf(arg,"%u",&c);
-set_chipset(c);
+  // Get the chipset code from the argument
+  sscanf(arg, "%u", &c);
+  set_chipset(c);
 }
 
 //****************************************************************
-//*   Установка параметров контрллера по типу чипсета
+//* Set controller parameters based on the chipset type
 //****************************************************************
 void set_chipset(unsigned int c)
 {
     if(maxchip == -1 && !load_config())
-        exit(0); // конфиг не загрузился
+        exit(0); // Configuration did not load
 
-    // получаем размер массива чипсетов
+    // Get the size of the chipset array
     for(int i = 0; i < maxchip ;i++)
-        // проверяем наш номер
+        // Check our number
         if(chipset[i].id == c)
             chip_type = i;
 
     if (chip_type < 0)
     {
-        printf("\n - Неверный код чипсета - %d\n", c);
+        printf("\n - Invalid chipset code - %d\n", c);
         exit(1);
     }
 
-    // устанавливаем адреса регистров чипсета
+    // Set controller register addresses
     #define setnandreg(name) name = chipset[chip_type].nandbase + nandreg[chipset[chip_type].ctrl_type].name;
     setnandreg(nand_cmd)
     setnandreg(nand_addr0)
@@ -312,53 +309,51 @@ void set_chipset(unsigned int c)
 }
 
 //**************************************************************
-//* Получение имени текущего чипсета
+//* Get the name of the current chipset
 //**************************************************************
 unsigned char* get_chipname() {
   return chipset[chip_type].name;
-}  
+}
 
 //**************************************************************
-//* Получение типа nand-контроллера
+//* Get the NAND controller type
 //**************************************************************
 unsigned int get_controller() {
   return chipset[chip_type].ctrl_type;
-}  
+}
 
 //**************************************************************
-//* Получение флага sahara-протокола
+//* Get the Sahara protocol flag
 //**************************************************************
 unsigned int get_sahara() {
   return chipset[chip_type].sahara;
-}  
+}
 
 //************************************************************
-//*  Проверка имени чипсета
+//* Check the chipset name
 //************************************************************
 int is_chipset(char* name) {
-  if (strcmp(chipset[chip_type].name,name) == 0) return 1;
+  if (strcmp(chipset[chip_type].name, name) == 0) return 1;
   return 0;
 }
 
-
 //**************************************************************
-//* Получение udsize
+//* Get udsize
 //**************************************************************
 unsigned int get_udflag() {
   return chipset[chip_type].udflag;
-}  
+}
 
 //**************************************************************
-//* Получение имени загрузчика NPRG
+//* Get the default NPRG bootloader name
 //**************************************************************
 char* get_nprg() {
   return chipset[chip_type].nprg;
-}  
+}
 
 //**************************************************************
-//* Получение имени загрузчика ENPRG
+//* Get the default ENPRG bootloader name
 //**************************************************************
 char* get_enprg() {
   return chipset[chip_type].enprg;
-}  
-
+}
