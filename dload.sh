acd6e95a7a3065fb51007a6f6bda6c21528e45ae
Aditya Singh <yoAeroA00@users.noreply.github.com>
2023-11-03 02:06:54 +0530

    Update *.sh : Proofreading

--- dload.sh
+++ dload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# Script for loading a bootloader into a modem in standard mode
+# Script for flashing a bootloader into a modem in standard mode
 #
-# Script parameter - tty port name, by default - /dev/ttyUSB0
+# Script parameter - tty port name, default is /dev/ttyUSB0
 #
 PORT=$1
 if [ -z "$PORT" ]; then PORT=/dev/ttyUSB0; fi
@@ -10,33 +10,32 @@ if [ -z "$PORT" ]; then PORT=/dev/ttyUSB0; fi
 LDR=$2
 if [ -z "$LDR" ]; then LDR=loaders/NPRG9x15p.bin; fi
 
-
 echo Diagnostic port: $PORT
 
-# Waiting for the port to appear on the system
+# Wait for the port to appear in the system
 while [ ! -c $PORT ]
  do
   sleep 1
  done
 
-# download mode command
+# Command to switch to download mode
 echo entering download mode...
 ./qcommand -p $PORT -e -c "c 3a" >/dev/null
 
-# Waiting for the port to disappear from the system
+# Wait for the port to disappear from the system
 while [ -c $PORT ]
  do
   true
  done
 echo diagnostic port removed
 
-# Waiting for the new port to arrive, already downloading
+# Wait for the appearance of a new port, now in download mode
 while [ ! -c $PORT ]
  do
   sleep 1
  done
 
-# Uploading the bootloader
+# Flash the bootloader
 echo download mode entered
 sleep 2
 ./qdload -t -p $PORT -i  $LDR
