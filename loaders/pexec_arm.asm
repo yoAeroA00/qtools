df90ab18a842537002a6c80d2bd9a230b82f2438
Aditya Singh <yoAeroA00@users.noreply.github.com>
2023-11-03 15:20:32 +0530

    Translate loaders\*.asm : To English

--- loaders/pexec_arm.asm
+++ loaders/pexec_arm.asm
@@ -1,5 +1,5 @@
 @
-@  Патч команды 11 для загрузчиков, работающих в режиме ARM
+@  Command 11 patch for loaders running in ARM mode
 @
 .syntax unified
 
@@ -13,19 +13,19 @@ leavecmd:
 
 cmd_11_exec:
 		PUSH    {R4,LR}
-		LDR     R1, reply_buf_ptr               @ Адрес ответнго буфера
-		ADD     R0, R0,	#8                      @ R0 теперь показывает на байт +4 от начал
-                                                @ командного буфера
-		MCR     p15, 0, R1,c7,c5,0              @ сброс I-кеша
-		BLX     R0                              @ передаем туда управление
-		LDR     R0, escape_state_ptr            @  Структура данных командного обработчика
-		STRH    R4, [R0,#pkt_data_len_off]      @ сохраняем размер ответного буфера
+		LDR     R1, reply_buf_ptr               @ Address of the reply buffer
+		ADD     R0, R0,	#8                      @ R0 now points to a byte +4 from the beginning
+                                                @ of the command buffer
+		MCR     p15, 0, R1,c7,c5,0              @ Flush I-cache
+		BLX     R0                              @ Pass control there
+		LDR     R0, escape_state_ptr            @ Command handler data structure
+		STRH    R4, [R0,#pkt_data_len_off]      @ Save the size of the reply buffer
 		B       leavecmd
 
-@ блок идентификации
+@ Identification block
 
-        .word           0xdeadbeef          @ сигнатура
-        .byte           N                   @ код чипсета
+        .word           0xdeadbeef          @ Signature
+        .byte           N                   @ Chipset code
 
 .ORG    0xXXXXXXXX
 
--- loaders/pexec_thumb.asm
+++ loaders/pexec_thumb.asm
@@ -1,5 +1,5 @@
 @
-@  Патч команды 11 для загрузчиков, работающих в режиме Thumb-2
+@  Command 11 patch for loaders running in Thumb-2 mode
 @
 .syntax unified
 .code 16           @  Thumb-2
@@ -14,19 +14,19 @@ leavecmd:
 
 cmd_11_exec:
         PUSH    {R4,LR}
-        LDR.N   R1, cmd_reply_code_ptr          @ Адрес ответнго буфера
-        ADD     R0, R0,    #8                   @ R0 теперь показывает на байт +4 от начала
-                                                @ командного буфера
-        MCR     p15, 0, R1,c7,c5,0              @ сброс I-кеша
-        BLX     R0                              @ передаем туда управление
-        LDR.N   R0, cmd_processor_data_ptr      @  Структура данных командного обработчика
-        STRH    R4, [R0,#pkt_data_len_off]      @ сохраняем размер ответного буфера
+        LDR.N   R1, cmd_reply_code_ptr          @ Address of the reply buffer
+        ADD     R0, R0,    #8                   @ R0 now points to a byte +4 from the beginning
+                                                @ of the command buffer
+        MCR     p15, 0, R1,c7,c5,0              @ Flush I-cache
+        BLX     R0                              @ Pass control there
+        LDR.N   R0, cmd_processor_data_ptr      @ Command handler data structure
+        STRH    R4, [R0,#pkt_data_len_off]      @ Save the size of the reply buffer
         B       leavecmd
 
-@ блок идентификации
+@ Identification block
 
-        .word           0xdeadbeef          @ сигнатура
-        .byte           N                   @ код чипсета
+        .word           0xdeadbeef          @ Signature
+        .byte           N                   @ Chipset code
 
 .ORG    0xXXXXXXXX
 
